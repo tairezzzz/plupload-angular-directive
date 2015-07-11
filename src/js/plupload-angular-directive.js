200ade66666d0f88c7f0c0bfdcd05ecec0593f99
tairezzzz <tairezzzz@gmail.com>
2015-07-11 17:31:12 +0300

    init delay for uploader

--- src/js/plupload-angular-directive.js
+++ src/js/plupload-angular-directive.js
@@ -29,7 +29,7 @@ angular.module('plupload.directive', [])
 		}];
 
 	})	
-	.directive('plUpload', ['$parse', '$log', 'plUploadService', function ($parse, $log, plUploadService) {
+	.directive('plUpload', ['$parse', '$log', 'plUploadService', '$timeout', function ($parse, $log, plUploadService, $timeout) {
 		return {
 			restrict: 'A',
 			scope: {
@@ -99,130 +99,132 @@ angular.module('plupload.directive', [])
 					options.multipart_params = scope.plMultiParamsModel;
 				}
 
+                var initDelay = iAttrs.plInitDelay.toLowerCase() == 'true'?300:0
 
-				var uploader = new plupload.Uploader(options);
+				$timeout(function(){
+                    var uploader = new plupload.Uploader(options);
 
-				uploader.settings.headers = plUploadService.getConfig('headers');
+                    uploader.settings.headers = plUploadService.getConfig('headers');
 
-				uploader.init();
+                    uploader.init();
 
-				uploader.bind('Error', function(up, err) {
-					if(iAttrs.onFileError){
-						scope.$parent.$apply(iAttrs.onFileError);
-					} 
-					
-					$log.error("Cannot upload, error: " + err.message + (err.file ? ", File: " + err.file.name : "") + "");
+                    uploader.bind('Error', function(up, err) {
+                        if(iAttrs.onFileError){
+                            scope.$parent.$apply(iAttrs.onFileError);
+                        }
 
-					up.refresh(); // Reposition Flash/Silverlight
- 				});
+                        $log.error("Cannot upload, error: " + err.message + (err.file ? ", File: " + err.file.name : "") + "");
 
-				uploader.bind('FilesAdded', function(up,files) {
-					//uploader.start();
-					scope.$apply(function() {
-						if(iAttrs.plFilesModel) {
-							angular.forEach(files, function(file,key) {
-								if (!scope.plFilesModel) scope.plFilesModel=[];
-								scope.plFilesModel.push(file);
-							});
-						}
-							
-						if(iAttrs.onFileAdded){
-							var fn = $parse(iAttrs.onFileAdded);
-							fn(scope.$parent, {$files:files});
-						}
-					});
+                        up.refresh(); // Reposition Flash/Silverlight
+                    });
 
-					if(iAttrs.plAutoUpload=="true"){
-						uploader.start();
-					}
-				});
-				
-                uploader.bind('BeforeUpload', function(up, file) {
-                    if(iAttrs.onBeforeUpload){
-                        var fn = $parse(iAttrs.onBeforeUpload);
-               			fn(scope.$parent, {$file:file});
+                    uploader.bind('FilesAdded', function(up,files) {
+                        //uploader.start();
+                        scope.$apply(function() {
+                            if(iAttrs.plFilesModel) {
+                                angular.forEach(files, function(file,key) {
+                                    if (!scope.plFilesModel) scope.plFilesModel=[];
+                                    scope.plFilesModel.push(file);
+                                });
+                            }
+
+                            if(iAttrs.onFileAdded){
+                                var fn = $parse(iAttrs.onFileAdded);
+                                fn(scope.$parent, {$files:files});
+                            }
+                        });
+
+                        if(iAttrs.plAutoUpload=="true"){
+                            uploader.start();
+                        }
+                    });
+
+                    uploader.bind('BeforeUpload', function(up, file) {
+                        if(iAttrs.onBeforeUpload){
+                            var fn = $parse(iAttrs.onBeforeUpload);
+                            fn(scope.$parent, {$file:file});
+                        }
+                    });
+
+                    uploader.bind('FileUploaded', function(up, file, res) {
+                            //We are going to make some refactor here.
+                                    //The idea behind is always update files with the server response value
+                                    //And also launch the eventi if neeed
+
+                                    //If we have the model...
+                                    if(iAttrs.plFilesModel) {
+                                        //Apply on scope...
+                                        scope.$apply(function() {
+
+                                            //All files are uploaded?
+                                            scope.allUploaded = false;
+
+                                            angular.forEach(scope.plFilesModel, function($file, key) {
+
+                                                //Bug FIX, this logic will set allUploaded right
+                                                if(file.percent != 100) {
+                                                    scope.allUploaded = false;
+                                                } else if(file.id == $file.id) { //If the file is the same that we are reciving...
+                                                    //Set response on the file
+                                                    $file.response = JSON.parse(res.response);
+
+                                                    //Need throw event? throw it
+                                                    if(iAttrs.onFileUploaded) {
+                                                        var fn = $parse(iAttrs.onFileUploaded);
+                                                        fn(scope.$parent, {$response:res, $file:file});
+                                                    }
+                                                }
+
+                                            });
+                                        });
+                                    }
+                                    //We doesn't have model but we have the event
+                                    else if(!iAttrs.plFilesModel && iAttrs.onFileUploaded) {
+                                        var fn = $parse(iAttrs.onFileUploaded);
+                                        scope.$apply(function(){
+                                            fn(scope.$parent, {$response:res, $file:file});
+                                        });
+                                    }
+                    });
+
+                    uploader.bind('UploadProgress',function(up,file){
+                        if(!iAttrs.plProgressModel){
+                            return;
+                        }
+
+                        if(iAttrs.plFilesModel){
+                            scope.$apply(function() {
+                                scope.sum = 0;
+
+                                angular.forEach(scope.plFilesModel, function(file,key) {
+                                    scope.sum = scope.sum + file.percent;
+                                });
+
+                                scope.plProgressModel = scope.sum/scope.plFilesModel.length;
+                            });
+                        } else {
+                            scope.$apply(function() {
+                                scope.plProgressModel = file.percent;
+                            });
+                        }
+
+
+                        if(iAttrs.onFileProgress){
+                            var fn = $parse(iAttrs.onFileProgress);
+                            scope.$apply(function(){
+                                fn(scope.$parent, {$file:file});
+                            });
+                        }
+                    });
+
+                    if(iAttrs.plInstance){
+                        scope.plInstance = uploader;
                     }
-                });
 
-				uploader.bind('FileUploaded', function(up, file, res) {
-					    //We are going to make some refactor here.
-			                    //The idea behind is always update files with the server response value
-			                    //And also launch the eventi if neeed
-			
-			                    //If we have the model...
-			                    if(iAttrs.plFilesModel) {
-			                        //Apply on scope...
-			                        scope.$apply(function() {
-			
-			                            //All files are uploaded?
-			                            scope.allUploaded = false;
-			
-			                            angular.forEach(scope.plFilesModel, function($file, key) {
-			
-			                                //Bug FIX, this logic will set allUploaded right
-			                                if(file.percent != 100) {
-			                                    scope.allUploaded = false;
-			                                } else if(file.id == $file.id) { //If the file is the same that we are reciving...
-			                                    //Set response on the file
-			                                    $file.response = JSON.parse(res.response);
-			
-			                                    //Need throw event? throw it
-			                                    if(iAttrs.onFileUploaded) {
-			                                        var fn = $parse(iAttrs.onFileUploaded);
-			                                        fn(scope.$parent, {$response:res, $file:file});
-			                                    }
-			                                }
-			
-			                            });
-			                        });
-			                    }
-			                    //We doesn't have model but we have the event
-			                    else if(!iAttrs.plFilesModel && iAttrs.onFileUploaded) {
-			                        var fn = $parse(iAttrs.onFileUploaded);
-			                        scope.$apply(function(){
-			                            fn(scope.$parent, {$response:res, $file:file});
-			                        });
-			                    }
-				});
-
-				uploader.bind('UploadProgress',function(up,file){
-					if(!iAttrs.plProgressModel){
-						return;
-					}
-					
-					if(iAttrs.plFilesModel){
-						scope.$apply(function() {
-							scope.sum = 0;
-
-							angular.forEach(scope.plFilesModel, function(file,key) {
-								scope.sum = scope.sum + file.percent;
-							});
-
-							scope.plProgressModel = scope.sum/scope.plFilesModel.length;
-						});
-					} else {
-						scope.$apply(function() {
-							scope.plProgressModel = file.percent;
-						});
-					}
-
-
-					if(iAttrs.onFileProgress){
-						var fn = $parse(iAttrs.onFileProgress);
-						scope.$apply(function(){
-							fn(scope.$parent, {$file:file});
-						});
-					}
-				});
-
-				if(iAttrs.plInstance){
-					scope.plInstance = uploader;	
-				}
-
-				scope.$on("$destroy", function(){
-                    			uploader.destroy();
-                		});
-                
+                    scope.$on("$destroy", function(){
+                        uploader.destroy();
+                    });
+                }, initDelay);
 			}
 		};
 	}])
